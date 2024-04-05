780eafe565f8af090c6fec84befd97badc0bd0cf
GhoRid <pgh5489@gmail.com>
2024-04-05 15:12:33 +0900

    chore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import MyPage from "./pages/MyPage/MyPage";
-import MenuBarLayOut from "./Layouts/MenuBarLayOut";
+import MenuBarLayout from "./Layouts/MenuBarLayout";
 import Layout from "./Layouts/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<MenuBarLayOut />}>
+        <Route element={<MenuBarLayout />}>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/mypage" element={<MyPage />}></Route>
         </Route>
