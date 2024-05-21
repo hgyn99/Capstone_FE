d7d9f3abdbcfb7c0ee971fb9cec34a06cc84d37e
raeau <raeau@MacBook-Air.local>
2024-05-21 14:12:20 +0900

    design: 출발지 입력창 디자인

--- src/pages/PathPage/SearchingBar.jsx
+++ src/pages/PathPage/SearchingBar.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import crossIcon from "../../assets/icon/cross.webp";
 import backwardIcon from "../../assets/icon/backwardIcon.webp";
+import pinIcon from "../../assets/icon/pinIcon.webp";
+import pantoIcon from "../../assets/icon/pantoIcon.webp";
 import { Link } from "react-router-dom";
 import React, { useState } from 'react';
 
@@ -16,11 +18,27 @@ const MainContainer = styled.div`
   //border-bottom: 2px solid ${(props) => props.theme.gray};
 `;
 
-const InputBox = styled.div`
+const InputBox1 = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: left;
   align-items: center;
+  color: ${(props) => props.theme.gray};
+  height: 40px;
+  //background-color: blue;
+  //padding: 0 0px; // 상하 여백 0, 좌우 여백 10px
+  //flex: 9;
+`;
+const InputBox2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: left;
+  align-items: center;
+  color: gray;
+  font-size: 15px;
+  font-weight: 600;
+  height: 40px;
+  gap: 10px;
   //background-color: blue;
   //padding: 0 0px; // 상하 여백 0, 좌우 여백 10px
   //flex: 9;
@@ -39,6 +57,10 @@ const InputButton = styled.button`
   width: 80%;
   border-radius: 3px;
   color: gray;
+  &:focus {
+    outline: none; // 기본 테두리 스타일 제거
+    border: 2px solid ${(props) => props.theme.blue}; // 테두리 스타일 설정
+  }
   &:focus::placeholder {
     color: transparent;
   }
@@ -50,8 +72,8 @@ const CloseButton = styled.button`
   background-position: center;
   background-size: contain;
   border: none;
-  width: 50px; // Adjust as needed
-  height: 50px; // Adjust as needed
+  width: 50px; 
+  height: 50px;
 `;
 const BackwardButton = styled.button`
   background-image: url(${backwardIcon});
@@ -60,6 +82,27 @@ const BackwardButton = styled.button`
   background-size: contain;
   border: none;
   margin-left: 10px;
+  width: 25px;
+  height: 25px;
+`;
+
+const PantoButton = styled.button`
+  margin-left: 40px;
+  background-image: url(${pantoIcon});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  border: none;
+  width: 25px;
+  height: 25px;
+`;
+
+const PinButton = styled.button`
+  background-image: url(${pinIcon});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  border: none;
   width: 25px; // Adjust as needed
   height: 25px; // Adjust as needed
 `;
@@ -92,12 +135,12 @@ const SearchingBar = () => {
   };
   return (
     <MainContainer>
-    <InputBox>
+    <InputBox1>
       {isDepartureInputClicked ? (
         <InputButton     
         as="input"
         type="text"
-        placeholder="출발지 입력"
+        //placeholder="출발지 입력"
         value={departureInput}
         onChange={handleInputChange}
         onKeyDown={handleKeyPress}
@@ -112,14 +155,20 @@ const SearchingBar = () => {
           <CloseButton />
         </Link>
       )}
-    </InputBox>
-      <InputBox>
-        {isDepartureInputClicked ? (
-          <InvisibleButton />
-        ) : (
-          <InputButton>도착지 입력</InputButton>
-        )}
-      </InputBox>
+    </InputBox1>
+    {/* <InputBox2 center={isDepartureInputClicked}> */}
+    <InputBox2>
+      {isDepartureInputClicked ? (
+        <>
+          <PantoButton />
+          <span style={{ marginRight: '40px' }}>현재 위치</span>
+          <PinButton />
+          <span>지도에서 선택</span>
+        </>
+      ) : (
+        <InputButton>도착지 입력</InputButton>
+      )}
+    </InputBox2>
     </MainContainer>
   );
 };
