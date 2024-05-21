e0967ce43abc2c01a783521c610ce10534ac700b
raeau <raeau@MacBook-Air.local>
2024-05-21 11:37:59 +0900

    feature: 출발지 입력 클릭시 처리

--- src/pages/PathPage/SearchingBar.jsx
+++ src/pages/PathPage/SearchingBar.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import crossIcon from "../../assets/icon/cross.webp";
+import backwardIcon from "../../assets/icon/backwardIcon.webp";
 import { Link } from "react-router-dom";
+import React, { useState } from 'react';
 
 const MainContainer = styled.div`
   margin-top: 10px;
@@ -27,7 +29,9 @@ const InputBox = styled.div`
 const InputButton = styled.button`
   background-color: #f0f0f0;
   border: none;
-  padding: 15px;
+  //padding: 15px;
+  text-indent: 10px;
+  height: 40px;
   margin-left: 20px;
   //margin-top: 10px;
   //margin-bottom: 10px;
@@ -35,6 +39,9 @@ const InputButton = styled.button`
   width: 80%;
   border-radius: 3px;
   color: gray;
+  &:focus::placeholder {
+    color: transparent;
+  }
 `;
 
 const CloseButton = styled.button`
@@ -46,18 +53,72 @@ const CloseButton = styled.button`
   width: 50px; // Adjust as needed
   height: 50px; // Adjust as needed
 `;
+const BackwardButton = styled.button`
+  background-image: url(${backwardIcon});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  border: none;
+  margin-left: 10px;
+  width: 25px; // Adjust as needed
+  height: 25px; // Adjust as needed
+`;
+
+const InvisibleButton = styled(InputButton)`
+  visibility: hidden;
+`;
 
 const SearchingBar = () => {
+  const [isDepartureInputClicked, setDepartureInputClicked] = useState(false);
+  const [departureInput, setDepartureInput] = useState("");
+
+  const handleDepartureInputClick = () => {
+    setDepartureInputClicked(true);
+  };
+
+  const handleBackwardButtonClick = (event) => {
+    event.stopPropagation();
+    setDepartureInputClicked(false);
+  };
+
+  const handleInputChange = (event) => {
+    setDepartureInput(event.target.value);
+  };
+  
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      console.log(departureInput);
+    }
+  };
   return (
     <MainContainer>
-      <InputBox>
-        <InputButton>출발지 입력</InputButton>{" "}
+    <InputBox>
+      {isDepartureInputClicked ? (
+        <InputButton     
+        as="input"
+        type="text"
+        placeholder="출발지 입력"
+        value={departureInput}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
+         />
+      ) : (
+        <InputButton onClick={handleDepartureInputClick}>출발지 입력</InputButton>
+      )}
+      {isDepartureInputClicked ? (
+        <BackwardButton onClick={handleBackwardButtonClick} />
+      ) : (
         <Link to="/">
           <CloseButton />
         </Link>
-      </InputBox>
+      )}
+    </InputBox>
       <InputBox>
-        <InputButton>도착지 입력</InputButton>
+        {isDepartureInputClicked ? (
+          <InvisibleButton />
+        ) : (
+          <InputButton>도착지 입력</InputButton>
+        )}
       </InputBox>
     </MainContainer>
   );
