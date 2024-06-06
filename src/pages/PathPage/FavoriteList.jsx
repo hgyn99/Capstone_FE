cacc1aae5bb015fc972369a2d3bc180b4d228efc
raeau <raeau@MacBook-Air.local>
2024-06-06 20:10:03 +0900

    feature: 즐겨찾기 장소 받아오기

--- src/pages/PathPage/FavoriteList.jsx
+++ src/pages/PathPage/FavoriteList.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import favoritePlace from "../../assets/icon/favoritePlaceIcon.webp";
+import { useQuery } from "@tanstack/react-query";
+import { fetchFavoritePath } from "../../apis/api/paths";
 
 const MainContainer = styled.div`
   border-top: 5px solid ${(props) => props.theme.gray};
@@ -56,23 +58,48 @@ const FavoriteIcon = styled.button`
 // FavoriteList 서버에서 받아오기
 
 const FavoriteList = () => {
-  const favorites = [
-    "전남대학교 광주캠퍼스 정문",
-    "광주종합터미널",
-    "북구청",
-    "공과대학 7호관",
-    "스타벅스 전남대점",
-    "전남대학교 스포츠센터",
-  ];
+  const {
+    isLoading,
+    data: favoritePathData, // 수정
+    refetch: favoritePathRefetch, // 수정
+  } = useQuery({
+    queryKey: ["favoritePath"],
+    queryFn: () => fetchFavoritePath(),
+    //enabled: !!address, // 수정
+    // keepPreviousData: true,
+    // staleTime: 5000,
+    onError: (e) => {
+      console.log(e);
+    },
+  });
+
+  //console.log(favoritePathData?.data.data);
+
+  // const favorites = [
+  //   "전남대학교 광주캠퍼스 정문",
+  //   "광주종합터미널",
+  //   "북구청",
+  //   "공과대학 7호관",
+  //   "스타벅스 전남대점",
+  //   "전남대학교 스포츠센터",
+  // ];
+
+  const favoriteRoutes = favoritePathData?.data.data.favoriteRoutes.map(
+    (favoriteRoute) => ({
+      id: favoriteRoute.id,
+      name: favoriteRoute.name,
+      // 여기에 서버에서 받아온 값 중 필요한 값 추가
+    })
+  );
 
   return (
     <MainContainer>
       <TitleBox>내 즐겨찾기 장소</TitleBox>
       <FavoriteListBox>
-        {favorites.map((favorite, index) => (
+        {favoriteRoutes.map((favoriteRoute, index) => (
           <FavoriteItem key={index}>
             <FavoriteIcon />
-            {favorite}
+            {favoriteRoute.name}
           </FavoriteItem>
         ))}
       </FavoriteListBox>
