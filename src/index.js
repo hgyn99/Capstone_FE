8498567797127f93cbd3e9f75b382f35eb91aa7d
GhoRid <pgh5489@gmail.com>
2024-06-10 19:00:22 +0900

    feat: service worker 수정

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ import theme from "./theme";
 import { RecoilRoot } from "recoil";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-serviceWorkerRegistration.register();
-reportWebVitals();
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const GlobalStyle = createGlobalStyle`
@@ -122,4 +119,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.register();
 reportWebVitals();
